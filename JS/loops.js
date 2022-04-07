04c7563c13b342c3b8e0cde22b56084c81ce88f5
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-07 10:19:23 -0300

    Loops

---
+++ JS/loops.js
@@ -0,0 +1,33 @@
+console.log(`Trabalhando com Condicionais\n  `)
+
+const listasDeDestinos = new Array(
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro',
+    'Curitiba'
+);
+//Variaveis do sistema:
+const idadeComprador = 13;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Curitiba";
+
+console.log(`\nDestinos Possiveis:`);
+console.log(listasDeDestinos);
+
+// está falando que pode comprar se um desses dois itens for verdadeiro, está jogando essa verificacao dentro de uma variavel, ao invez de usar o if
+const podeComprar = idadeComprador >= 18 || estaAcompanhada === true
+
+let contador = 0;
+let destinoExiste = false;
+
+while(contador < 4){
+    // está rodando um if, que está verificando se há o destino que a pessoa quer, se não estiver ele n printa nada, vai passar a lista inteira e vai procurar pelo destino da pessoa 
+    if(listasDeDestinos[contador] == destino){
+        destinoExiste = true;
+        break; 
+    }
+    contador ++;
+}
+
+console.log("Destino Existe:", destinoExiste);

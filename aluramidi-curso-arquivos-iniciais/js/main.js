f9694b82434be6f00cd96ec533f7c8bb87157521
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-18 20:34:36 -0300

    update

---
+++ aluramidi-curso-arquivos-iniciais/js/main.js
@@ -0,0 +1,14 @@
+const btnPom = document.getElementsByClassName('.tecla_pom');
+/*const btnClap
+const btnTim
+const btnPuff
+const btnSplash
+const btnToim
+const btnPsh
+const btnTic
+const btnTom
+*/
+
+btnPom.addEventListener('click', () => {
+alert('voce clicou pum');
+})

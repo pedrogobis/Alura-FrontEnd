53a88240f82b021f88c57edb8ccb5f8ff63c68e0
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-19 13:32:51 -0300

    Midi funcionando

--- aluramidi-curso-arquivos-iniciais/js/main.js
+++ aluramidi-curso-arquivos-iniciais/js/main.js
@@ -1,6 +1,6 @@
 
-function tocaSom(idElementoAudio){
-    document.querySelectorAll(idElementoAudio).play()
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,9 +8,20 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let i = 0;
 
 while(i < listaDeTeclas.length ){
-    listaDeTeclas[i].onclick = tocaSom;
+    //para evitar codigo repetido, colocamos uma referencia, para n ficar duplicando.
+    const tecla = listaDeTeclas[i];
+    // assim como no console, ela esta acessando a lista de teclas, e acessando a classe lista, com o indice 1, então vai ser o segundo valor.
+    const instrumento = tecla.classList[1];
+    
+    // ela criou o template, para preencher o resto do id, pois a classe do botão é tecla_somx e o id de cada som é som_tecla_somx, então desse jeito ela referenciou tudo
+    const idAudio = `#som_${instrumento}`
+    
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
     i++
-
+    
 }
 /*
 btnPom.EventListener('click', () => {

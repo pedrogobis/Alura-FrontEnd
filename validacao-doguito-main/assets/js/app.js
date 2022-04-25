617073e1d3c7b385eaa89242bf1708769f378eb9
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-25 15:11:44 -0300

    Validacao nao esta funcionando

---
+++ validacao-doguito-main/assets/js/app.js
@@ -0,0 +1,11 @@
+import { valida } from './validacao.js'
+
+//pegou todos os inputs
+const inputs = document.querySelectorAll('input')
+
+// aqui ele criou um for each, ou seja para cada input dentro de inputs, ele vai chamar o addeventilisnier no blur e chamar a funcao valida, tipo o que a agente tinha feito no arquivo anterior, porém de um jeito mais generico para poder ser utilizado mais vezes. 
+inputs.forEach(input =>{
+    input.addEventListener(blur, (evento) =>{
+        valida(evento.target)
+    } )
+} )
--- validacao-doguito-main/assets/js/validacao.js
+++ validacao-doguito-main/assets/js/validacao.js
@@ -1,7 +1,25 @@
+export function valida(input){
+    // estamos declarando uma variavel que vai receber o dataatributes que criamos no html, veja que no dataset, colocamos ponto. tipo, que é o que colocamos la no html
+    const tipoDeInput = input.dataset.tipo
+    if(validadores [tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+
+}
+// criamos um objeto que vai conter cada tipo de dados dentro do dataatributes, no caso aqui quando tiver um tipo dataNascimento, o input vai chamar o validaDataNascimento, com o valor do input.
+const validadores = {
+     dataNascimento: input => validaDataNascimento(input)
+}
+/*
+Refatorando o codigo, faremos uma funcao para usar todos os dados de maneira generica.
+no html usaremos o dataatributes
+
+
 const dataNascimento = document.querySelector('#nascimento')
 dataNascimento.addEventListener('blur', (evento) => {
     validaDataNascimento(evento.target)
 })
+*/
 
 function validaDataNascimento(input){
     const dataRecebida = new Date(input.value);

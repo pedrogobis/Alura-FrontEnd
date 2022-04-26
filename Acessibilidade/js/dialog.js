37fafd8ebdb32550592052f65fdd432236ef88e2
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-26 15:09:48 -0300

    update

--- Acessibilidade/js/dialog.js
+++ Acessibilidade/js/dialog.js
@@ -1,18 +1,25 @@
 // Variáveis
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
-var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');// esse é a transparencia do fundo do site.
 
+btnAbreDialog.style.display = 'block'
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true; // tirando o foco da pagina.
+  
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto'); 
+  conteudoForaDialog.inert = false; //voltando o foco para pagina
+  btnAbreDialog.focus();
 }
 
 // Listeners

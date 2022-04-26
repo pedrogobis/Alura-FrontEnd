9a990e4921438f991b3df60149cf6a2381022cfc
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-26 16:28:46 -0300

    Terminando o dialogjs
    finalizado o js

--- Acessibilidade/js/dialog.js
+++ Acessibilidade/js/dialog.js
@@ -1,5 +1,6 @@
 // Variáveis
 var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var controleVideo = document.querySelector('video');
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
@@ -12,6 +13,7 @@ btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
   document.querySelector('.dialogNewsletter-campo').focus();
   conteudoForaDialog.inert = true; // tirando o foco da pagina.
+  controleVideo.removeAttribute('controls');
   
 });
 
@@ -20,7 +22,17 @@ function fechandoDialog() {
   dialog.classList.remove('dialogNewsletter--aberto'); 
   conteudoForaDialog.inert = false; //voltando o foco para pagina
   btnAbreDialog.focus();
+  controleVideo.setAttribute('controls', true);// setatribute adiciona
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+dialogOverlay.addEventListener('click', fechandoDialog);
+
+
+document.addEventListener('keyup', function(evento){
+  if(evento.keyCode == 27){// codigo do esq
+    fechandoDialog();
+
+  }
+});

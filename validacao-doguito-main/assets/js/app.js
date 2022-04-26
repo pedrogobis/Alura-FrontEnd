e58d21c114a87bda6884127aedd5cd48372680a3
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-26 10:25:42 -0300

    Finalizamos
    -Importamos uma mascara de dinheiro

--- validacao-doguito-main/assets/js/app.js
+++ validacao-doguito-main/assets/js/app.js
@@ -5,6 +5,17 @@ const inputs = document.querySelectorAll('input')
 
 // aqui ele criou um for each, ou seja para cada input dentro de inputs, ele vai chamar o addeventilisnier no blur e chamar a funcao valida, tipo o que a agente tinha feito no arquivo anterior, porém de um jeito mais generico para poder ser utilizado mais vezes. 
 inputs.forEach(input => {
+    if(input.dataset.tipo === 'preco'){// vai fazer uma vericiacao s eo o input do dataset tipo é igual ao preco, se for chama o simple mask se nao vai para o eventlistnner que dae usa a funcao valida do validacao.
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$ ',
+            suffix: '',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'})// vai mover o cursor para o final
+    }
+
     input.addEventListener('blur', (evento) =>{
         valida(evento.target)
     } )
--- validacao-doguito-main/assets/js/validacao.js
+++ validacao-doguito-main/assets/js/validacao.js
@@ -51,6 +51,9 @@ const  mensagensDeErro = {
      },
      estado:{
         valueMissing: 'O campo do estado não pode estar vazio.',
+     },
+     preco:{
+        valueMissing: 'O campo de preço não pode estar vazio.',
      }
 
 }

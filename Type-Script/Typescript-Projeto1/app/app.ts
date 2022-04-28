51dfe987ea2c3537cc876d46682f28d2e5fcd1fc
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-04-28 16:46:07 -0300

    seguindo

--- Type-Script/Typescript-Projeto1/app/app.ts
+++ Type-Script/Typescript-Projeto1/app/app.ts
@@ -1,5 +1,7 @@
 import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date, 10, 392); // new date vai pegar a data atual e colocar dentro.
+
+const negociacao = new Negociacao (new Date, 10, 392); // new date vai pegar a data atual e colocar dentro.
 console.log(negociacao);
+
 //negociacao.quantidade = 1000; // adicionou uma nova propriedade dinamicamente, mas não alterou a classe quantidade privada.
 console.log(negociacao.volume);

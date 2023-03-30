1d461a51826dbf75420340d2639157d1a7647571
pedro Gobis <pedro.gobis@gmail.com>
2023-03-30 16:31:18 -0300

    parte3

---
+++ Formação front-end 2023 update/Manipulando DOOM - novamente./Proejto/-robotron-2000-projeto_inicial/main.js
@@ -0,0 +1,9 @@
+let pegaoitem = document.getElementsByClassName('robo');
+
+pegaoitem.addEventLisnter("click", alert('Eu sou o chatgpt'))
+
+console.log('é fake, não é o chatgpt não.')
+
+function dizOi(){
+    console.log()
+}
